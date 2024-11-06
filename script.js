dde4844ba6f2aa5c95a89ab296408f4e497b02bd
brayyofc <zanssxploit@gmail.com>
2024-11-06 08:29:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function redirectToSocialMedia(platform) {
+    let url = "";
+
+    switch (platform) {
+        case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Instagram Anda
+            break;
+        case 'YouTube':
+            url = "https://www.youtube.com/wanzofc"; // Ganti dengan URL YouTube Anda
+            break;
+        case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+        case 'telegram':
+            url = "https://t.me/wanzofc"; // Ganti dengan URL Twitter Anda
+            break;
+            case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+            case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+            case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+            case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+            case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+            case 'whatsapp':
+            url = "https://wa.me/62895402567224"; // Ganti dengan URL Facebook Anda
+            break;
+        default:
+            url = "#";
+    }
+
+    window.open(url, '_blank');
+}
